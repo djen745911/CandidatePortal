8334612450b90593fc98393869abab882f0c684e
djen745911 <65673403+djen745911@users.noreply.github.com>
2025-05-15 20:00:10 +0100

    Deploy application to Netlify

---
+++ dist/assets/map-pin-37f7c9ce.js
@@ -0,0 +1,1 @@
+import{f as a}from"./index-8ded0fa8.js";const c=a("DollarSign",[["line",{x1:"12",x2:"12",y1:"2",y2:"22",key:"7eqyqh"}],["path",{d:"M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6",key:"1b0p4s"}]]),i=a("MapPin",[["path",{d:"M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z",key:"2oe9fu"}],["circle",{cx:"12",cy:"10",r:"3",key:"ilqhr7"}]]);export{c as D,i as M};
